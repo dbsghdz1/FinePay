e2590ad688924a147f9cea5d983bd4cdbd9316b9
Hong <dbsghdz1@gmail.com>
2025-04-04 21:00:52 +0900

    Feat: 솔라나 보내는 로직 추가

--- FinePay/PhantomConnectExample/PhantomConnectExample/SendingSOL/View/SendingView.swift
+++ FinePay/PhantomConnectExample/PhantomConnectExample/SendingSOL/View/SendingView.swift
@@ -7,8 +7,25 @@
 
 import SwiftUI
 
+import Solana
+import PhantomConnect
+
 struct SendingView {
-  @State private var confirmButtonClicked = false
+  @State var confirmButtonClicked = false
+  let solCoin: Double
+  @StateObject var viewModel: PhantomConnectViewModel
+  let phantomEncryptionKey = UserDefaults.standard.string(forKey: "phantomEncryptionKey")
+  let walletPublicKey = UserDefaults.standard.string(forKey: "walletPublicKey")
+  let paduckWallet = "B9hNZruBjAQSdrYYvktnPgGTPsVbY82MyxCZcHraVsZZ"
+  var session = UserDefaults.standard.string(forKey: "session")
+  
+  var formattedSOLCoin: String {
+    if solCoin.truncatingRemainder(dividingBy: 1) == 0 {
+      return String(format: "%.6f", solCoin).removeZeros()
+    } else {
+      return String(format: "%.6f", solCoin).removeZeros()
+    }
+  }
 }
 
 extension SendingView: View {
@@ -18,7 +35,7 @@ extension SendingView: View {
       Text("Sending")
         .font(.system(size: 40, weight: .semibold))
         .foregroundStyle(Color.textBlackColor)
-      Text("0.1 SOL")
+      Text("\(formattedSOLCoin) SOL")
         .font(.system(size: 40, weight: .semibold))
         .foregroundStyle(Color.textBlackColor)
       HStack {
@@ -32,7 +49,19 @@ extension SendingView: View {
       Spacer()
       Button {
         confirmButtonClicked = true
-        print("buttonClicked")
+        createTransaction { serializedTransaction in
+          do {
+            try viewModel.sendAndSignTransaction(
+              serializedTransaction: serializedTransaction,
+              dappEncryptionKey: viewModel.linkingKeypair?.publicKey,
+              phantomEncryptionKey: PublicKey(string: phantomEncryptionKey ?? ""),
+              session: session ?? "",
+              dappSecretKey: viewModel.linkingKeypair?.secretKey
+            )
+          } catch {
+            print("sendAndSignTransaction Error: \(error.localizedDescription)")
+          }
+        }
       } label: {
         Text("Confirm")
           .font(.system(size: 26, weight: .semibold))
@@ -43,15 +72,59 @@ extension SendingView: View {
               .foregroundStyle(Color.mainColor)
           )
       }
+      .frame(width: 361, height: 55)
       .padding(.horizontal)
       .padding(.bottom, 38)
-      .navigationDestination(isPresented: $confirmButtonClicked) {
-        SendingView()
+      .onWalletTransaction(
+        phantomEncryptionPublicKey: PublicKey(string: phantomEncryptionKey ?? ""),
+        dappEncryptionSecretKey: viewModel.linkingKeypair?.secretKey
+      ) { signature, error in
+        if let error = error {
+          print("failTransaction : \(error.localizedDescription)")
+        } else if let signature = signature {
+          print("successTransaction: \(signature)")
+        }
+      }
+    }
+    .onAppear {
+      PhantomConnect.configure(
+        appUrl: "https://example.com",
+        cluster: "devnet",
+        redirectUrl: "example://"
+      )
+    }
+  }
+  
+  func createTransaction(completion: @escaping ((_ serializedTransaction: String) -> Void)) {
+    Task {
+      do {
+        let recentBlockhash = try await NetworkManager.shared.getBlockHash()
+        
+        _ = Solana(router: NetworkingRouter(endpoint: RPCEndpoint.devnetSolana))
+        
+        var transaction = Transaction(
+          feePayer: PublicKey(string: "8zvV1Gig5i1pHbyXbqEQBHTe2Ft8qvzR9CPziZQ5p3ET")!,
+          instructions: [
+            SystemProgram.transferInstruction(
+              from: PublicKey(string: "8zvV1Gig5i1pHbyXbqEQBHTe2Ft8qvzR9CPziZQ5p3ET")!,
+              to: PublicKey(string: paduckWallet)!,
+              lamports: 1000000000
+            )
+          ],
+          recentBlockhash: recentBlockhash
+        )
+        
+        let serializedTransaction = try transaction.serialize().get()
+        DispatchQueue.main.async {
+          completion(Base58.encode(serializedTransaction.bytes))
+        }
+      } catch {
+        print("blockHashError: \(error)")
       }
     }
   }
 }
 
 #Preview(body: {
-  SendingView()
+  SendingView(solCoin: 0.0, viewModel: PhantomConnectViewModel())
 })
