23d6f11b51c77cf0e15eeb35538d17a47c8a84ab
김민석|Minseok Kim <alstjr7437@naver.com>
2025-04-04 18:45:07 +0900

    feat: 특정 영역에 Radius를 주기 위한 Extension 추가

---
+++ FinePay/PhantomConnectExample/PhantomConnectExample/Extension/View+Ex.swift
@@ -0,0 +1,25 @@
+//
+//  View+Ex.swift
+//  FinePay
+//
+//  Created by 김민석 on 4/3/25.
+//
+
+import SwiftUI
+
+extension View {
+    func cornerRadius(_ radius: CGFloat, corners: UIRectCorner) -> some View {
+        clipShape(RoundedCorner(radius: radius, corners: corners))
+    }
+}
+
+struct RoundedCorner: Shape {
+    var radius: CGFloat = .infinity
+    var corners: UIRectCorner = .allCorners
+    
+    func path(in rect: CGRect) -> Path {
+        let path = UIBezierPath(roundedRect: rect, byRoundingCorners: corners, cornerRadii: CGSize(width: radius, height: radius))
+        
+        return Path(path.cgPath)
+    }
+}
