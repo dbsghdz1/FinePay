57f09cf3d95d1dfafbf30bd79c0238d9054c3406
Hong <dbsghdz1@gmail.com>
2025-04-05 18:26:41 +0900

    Fix: 결과화면 후 홈화면

--- FinePay/PhantomConnectExample/PhantomConnectExample/SendingSOL/View/SendingResultView.swift
+++ FinePay/PhantomConnectExample/PhantomConnectExample/SendingSOL/View/SendingResultView.swift
@@ -15,41 +15,45 @@ struct SendingResultView {
 
 extension SendingResultView: View {
   var body: some View {
-    VStack {
-      Spacer()
-      Image(systemName: "checkmark.circle.fill")
-        .resizable()
-        .frame(width: 96, height: 96)
-        .foregroundStyle(Color.mainColor)
-      Text("Sent \(solCoin)")
-        .font(.system(size: 40, weight: .semibold))
-      HStack {
-        Text("to")
-          .font(.system(size: 40, weight: .semibold))
-          .foregroundStyle(Color.textBlackColor)
-        Text("Hong")
-          .font(.system(size: 40, weight: .bold))
-          .foregroundStyle(Color.mainColor)
+      if !okayButtonClicked {
+          VStack {
+              Spacer()
+              Image(systemName: "checkmark.circle.fill")
+                  .resizable()
+                  .frame(width: 96, height: 96)
+                  .foregroundStyle(Color.mainColor)
+              Text("Sent \(solCoin)")
+                  .font(.system(size: 40, weight: .semibold))
+              HStack {
+                  Text("to")
+                      .font(.system(size: 40, weight: .semibold))
+                      .foregroundStyle(Color.textBlackColor)
+                  Text("Hong")
+                      .font(.system(size: 40, weight: .bold))
+                      .foregroundStyle(Color.mainColor)
+              }
+              //TODO: 메모?
+              Spacer()
+              Button {
+                  okayButtonClicked = true
+                  print("buttonClicked")
+              } label: {
+                  Text("Okay")
+                      .font(.system(size: 26, weight: .semibold))
+                      .foregroundStyle(.white)
+                      .background(
+                        RoundedRectangle(cornerRadius: 10)
+                            .frame(width: 361, height: 55)
+                            .foregroundStyle(Color.mainColor)
+                      )
+              }
+              .padding(.horizontal)
+              .padding(.bottom, 38)
+          }
+          .navigationBarBackButtonHidden(true)
+      } else {
+          HomeView()
       }
-      //TODO: 메모?
-      Spacer()
-      Button {
-        okayButtonClicked = true
-        print("buttonClicked")
-      } label: {
-        Text("Okay")
-          .font(.system(size: 26, weight: .semibold))
-          .foregroundStyle(.white)
-          .background(
-            RoundedRectangle(cornerRadius: 10)
-              .frame(width: 361, height: 55)
-              .foregroundStyle(Color.mainColor)
-          )
-      }
-      .padding(.horizontal)
-      .padding(.bottom, 38)
-    }
-    .navigationBarBackButtonHidden(true)
   }
 }
 
