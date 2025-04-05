b1c9dfb6957d4622e1d928a190510c7472eaaaa8
김민석|Minseok Kim <alstjr7437@naver.com>
2025-04-05 13:12:41 +0900

    refactor: ViewBuilder 함수화를 통해서 body부분의 가독성 변경

--- FinePay/PhantomConnectExample/PhantomConnectExample/Home/View/HomeView.swift
+++ FinePay/PhantomConnectExample/PhantomConnectExample/Home/View/HomeView.swift
@@ -15,16 +15,15 @@ struct HomeView: View {
     @State private var isSheetPresented = false
 
     var body: some View {
-        ZStack(
-            alignment: .bottom
-        ) {
+        ZStack(alignment: .bottom) {
             VStack {
                 PeerView(
                     peers: multipeerSession.foundPeers,
                     inviteAction: { peer in
                         withAnimation {
                             selectedPeer = peer
-                            sendingFromPeer = nil                        }
+                            sendingFromPeer = nil
+                        }
                     }
                 )
                 .padding()
@@ -32,55 +31,68 @@ struct HomeView: View {
                 BottomWalletView()
                     .padding(.bottom, 40)
             }
-            
-            if selectedPeer != nil || sendingFromPeer != nil {
-                Color.black.opacity(0.4)
-                    .ignoresSafeArea()
-                    .transition(.opacity)
-                    .onTapGesture {
-                        withAnimation {
-                            selectedPeer = nil
-                            sendingFromPeer = nil
-                        }
-                    }
-            }
-            
-            if let peer = selectedPeer {
-                InvitePopupView(peer: peer) {
-                    multipeerSession
-                        .invite(peer)
-                    withAnimation {
-                        selectedPeer = nil
-                    }
-                }
-                .transition(.move(edge: .bottom))
-                .animation(.easeInOut, value: selectedPeer)
-            }
-            if let peer = sendingFromPeer {
-                SendingWalletAddressPopupView(
-                    peer: peer,
-                    onSend: {
-                        print("Send wallet address to \(peer.id)")
-                        withAnimation {
-                            sendingFromPeer = nil
-                        }
-                    },
-                    reject: {
-                        print("Rejected \(peer.id)'s request")
-                        withAnimation {
-                            sendingFromPeer = nil
-                        }
-                    }
-                )
-                .transition(.move(edge: .bottom))
-            }
+
+            dimmedBackground()
+            invitePopup()
+            sendingWalletPopup()
         }
-        .animation(.easeInOut, value: selectedPeer)
-        .animation(.easeInOut, value: sendingFromPeer)
         .ignoresSafeArea()
     }
 }
 
+extension HomeView {
+    @ViewBuilder
+    private func dimmedBackground() -> some View {
+        if selectedPeer != nil || sendingFromPeer != nil {
+            Color.black.opacity(0.4)
+                .ignoresSafeArea()
+                .transition(.opacity)
+                .onTapGesture {
+                    withAnimation {
+                        selectedPeer = nil
+                        sendingFromPeer = nil
+                    }
+                }
+        }
+    }
+
+    @ViewBuilder
+    private func invitePopup() -> some View {
+        if let peer = selectedPeer {
+            InvitePopupView(peer: peer) {
+                multipeerSession.invite(peer)
+                withAnimation {
+                    selectedPeer = nil
+                }
+            }
+            .transition(.move(edge: .bottom))
+            .animation(.easeInOut, value: selectedPeer)
+        }
+    }
+
+    @ViewBuilder
+    private func sendingWalletPopup() -> some View {
+        if let peer = sendingFromPeer {
+            SendingWalletAddressPopupView(
+                peer: peer,
+                onSend: {
+                    print("Send wallet address to \(peer.id)")
+                    withAnimation {
+                        sendingFromPeer = nil
+                    }
+                },
+                reject: {
+                    print("Rejected \(peer.id)'s request")
+                    withAnimation {
+                        sendingFromPeer = nil
+                    }
+                }
+            )
+            .transition(.move(edge: .bottom))
+            .animation(.easeInOut, value: sendingFromPeer)
+        }
+    }
+}
 
 
 #Preview {
