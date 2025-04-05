0923e7eecd22652e09e01d0b4478093abec5b172
김민석|Minseok Kim <alstjr7437@naver.com>
2025-04-05 11:27:31 +0900

    feat: Invite 임시 View 구현

--- FinePay/PhantomConnectExample/PhantomConnectExample/Home/View/InviteView.swift
+++ FinePay/PhantomConnectExample/PhantomConnectExample/Home/View/InviteView.swift
@@ -1,19 +1,20 @@
 //
-//  SendView.swift
+//  InviteView.swift
 //  FinePay
 //
-//  Created by 김민석 on 4/3/25.
+//  Created by 김민석 on 4/4/25.
 //
+
 
 import SwiftUI
 
-struct SendView: View {
+struct InviteView: View {
     let peer: Peer
     let onSend: () -> Void
 
     var body: some View {
         VStack(spacing: 20) {
-            SentTextView(id: peer.id)
+            InviteTextView(id: peer.id)
 
             Button(action: {
                 onSend()
@@ -48,27 +49,20 @@ struct SendView: View {
 }
 
 
-struct SentTextView: View {
+struct InviteTextView: View {
     let id: String
     
     var body: some View {
-        VStack {
-            Text("Want to send")
-                .font(.system(size: 26, weight: .semibold))
-            
-            (
-                Text("Solana to ")
-                    .font(.system(size: 26, weight: .semibold))
-                +
-                Text(id) // or peer.displayName
-                    .font(.system(size: 26, weight: .bold))
-            )
-        }
-        .multilineTextAlignment(.center)
+        Text(id)
+            .font(.system(size: 26, weight: .bold))
+            .foregroundStyle(Color.mainColor)
+        +
+        Text(" want your wallet address")
+            .font(.system(size: 26, weight: .semibold))
     }
     
 }
 
 #Preview {
-    SendView(peer: Peer(id: "hello", wallet: ""), onSend: {} )
+    InviteView(peer: Peer(id: "hello", wallet: ""), onSend: {} )
 }
