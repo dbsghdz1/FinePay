293fa6f245edadf7fe1e446a1de4526c57e21a3a
Hong <dbsghdz1@gmail.com>
2025-04-05 16:42:29 +0900

    Feat: 토큰 잔액 가져오기

--- FinePay/PhantomConnectExample/PhantomConnectExample/Common/NetworkManager.swift
+++ FinePay/PhantomConnectExample/PhantomConnectExample/Common/NetworkManager.swift
@@ -39,4 +39,35 @@ final class NetworkManager {
     let blockHash = try JSONDecoder().decode(BlockHashModel.self, from: data)
     return blockHash.result.value.blockhash
   }
+  
+  func fetchSOLBalance(address: String) async throws -> String {
+    var urlComponents = URLComponents()
+    urlComponents.scheme = "https"
+    urlComponents.host = "api.devnet.solana.com"
+    var request = URLRequest(url: urlComponents.url!)
+    
+    request.httpMethod = HttpMethod.POST.rawValue
+    
+    let body: [String: Any] = [
+        "jsonrpc": "2.0",
+        "id": 1,
+        "method": "getBalance",
+        "params": [address]
+    ]
+    
+    request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+    request.httpBody = try? JSONSerialization.data(withJSONObject: body)
+    
+    do {
+      request.httpBody = try JSONSerialization.data(withJSONObject: body, options: [])
+    } catch {
+      print(error)
+    }
+    
+    let (data, _) = try await URLSession.shared.data(for: request)
+    let tokenInfo = try JSONDecoder().decode(TokenInfo.self, from: data)
+    let rawLamports = tokenInfo.result.value
+    let sol = Double(rawLamports) / 10_0000_0000
+    return sol.formatted()
+  }
 }
